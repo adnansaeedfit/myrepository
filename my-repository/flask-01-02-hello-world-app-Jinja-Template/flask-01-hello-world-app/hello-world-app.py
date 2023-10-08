87cbe037e5380d40c595034efac658ad3515cfce
Adnan Saeed <adnansaeedfit@gamil.com>
2023-10-08 00:52:26 -0400

    add hello world changes

---
+++ my-repository/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,56 @@
+# Hello World App
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
+@app.route("/second")
+def second():
+    return "<h2>This is the second page</h2>"
+
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"The id of this page is {id}"
+
+
+# Run the flask app
+app.run(debug=False)# Hello World App
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
+app.route("/second")
+def second():
+    return "<h2>This is the second page</h2>"
+
+app.route("/third")
+def third():
+    return "This is the third page"
+
+app.route("/forth/<string:id>")
+def forth(id):
+    if id.isdigit():
+        return f"The id of this page is {id}"
+    else:
+        return f"not a valid page id!"
+
+
+
+app.run(debug=False)
